2838e9a6ae05c072a69985b22f3e729f15bd1820
Susans Finkel <susan.finkel@outlook.com>
2017-03-06 15:12:26 +0000

    added pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
     
   end
+  
+  def about
+    
+  end
+  
+  def help
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
+  get '/about', to: 'pages#about'
+  get '/help', to: 'pages#help'
 end
